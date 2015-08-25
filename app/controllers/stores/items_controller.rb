df0677cc5479d22e7fbc9d37487d7de9ddd1f2a7
Adam Caron <adamwcaron@gmail.com>
2015-08-25 11:51:41 -0600

    adds multi-tenancy

---
+++ app/controllers/stores/items_controller.rb
@@ -0,0 +1,11 @@
+class Stores::ItemsController < Stores::StoresController
+  def index
+    @items = current_store.items
+  end
+
+  def show
+    @item = current_store.items.find_by(id: params[:id])
+
+    redirect_to store_items_path(store: current_store.url), notice: "Item not found" unless @item
+  end
+end
---
+++ app/models/store.rb
@@ -0,0 +1,13 @@
+class Store < ActiveRecord::Base
+  validates :name, presence: true
+  validates :url, presence: true, uniqueness: true
+
+  has_many :items
+  has_many :orders
+
+  before_validation :generate_url
+
+  def generate_url
+    self.url = name.parameterize
+  end
+end
